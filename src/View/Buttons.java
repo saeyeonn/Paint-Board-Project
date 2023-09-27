d2b4a5e671f9ee0e1a6da4e1ae12dcc5a6bb2359
saeyeon0522 <sy01lim@naver.com>
2023-09-28 01:55:13 +0900

    Add: add textbox settings and line settings Design: group color buttons and shape buttons

--- src/View/Buttons.java
+++ src/View/Buttons.java
@@ -2,8 +2,6 @@ package View;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 
@@ -28,22 +26,51 @@ public class Buttons {
         for (int i = 0; i < buttons.length; i++) {
             buttons[i] = new JButton(imageIcons[i]);
             container.add(buttons[i]);
-            buttons[i].setName("img" + iconList[i]);
+            buttons[i].setName("img-" + iconList[i]);
 
-            if (buttons[i].getName().equals("imgafter"))
-                container.add(Box.createHorizontalStrut(30));
+            if (buttons[i].getName().equals("img-after"))
+                container.add(Box.createHorizontalStrut(5));
+            if (buttons[i].getName().equals("img-textBox"))
+                container.add(Box.createVerticalStrut(50));
+            if (buttons[i].getName().equals("img-ungroup") || buttons[i].getName().equals("img-circle"))
+                container.add(Box.createVerticalStrut(70));
+            container.add(Box.createHorizontalStrut(2));
 
-            if (buttons[i].getName().equals("imgrectangularBox1"))
-                buttons[i].setPreferredSize(new Dimension(105, 35));
+            if (buttons[i].getName().equals("img-lineType")) {
+                buttons[i].setPreferredSize(new Dimension(180, 45));
+                buttons[i].setBorderPainted(true);
+
+                JComboBox<String> lineBoldComboBox = new JComboBox<>(new String[] {"line 3pt", "line 6pt", "line 9pt"});
+                lineBoldComboBox.setPreferredSize(new Dimension(180, 30));
+                container.add(lineBoldComboBox);
+            }
             else buttons[i].setPreferredSize(new Dimension(35, 35));
+
             buttons[i].setFocusPainted(false);
             buttons[i].setBorderPainted(false);
             buttons[i].setContentAreaFilled(false);
 
             clickAction(buttons[i], imageIcons[i]);
-
             if (iconList[i].equals("more")) selectColor(buttons[i], frame);
         }
+        createTextBoxSetting(container);
+    }
+
+    private void createTextBoxSetting(Container container) {
+
+        JComboBox<String> fontComboBox = new JComboBox<>(new String[] {"Arial", "Times New Roman", "Courier New"});
+        fontComboBox.setPreferredSize(new Dimension(90, 30));
+        container.add(fontComboBox);
+
+        SpinnerNumberModel fontSizeModel = new SpinnerNumberModel(12, 1, 80, 5);
+        JSpinner fontSizeSpinner = new JSpinner(fontSizeModel);
+        fontSizeSpinner.setPreferredSize(new Dimension(45, 30));
+        container.add(fontSizeSpinner);
+
+        JCheckBox fontBoldBox = new JCheckBox("B");
+        fontBoldBox.setOpaque(false);
+        fontBoldBox.setPreferredSize(new Dimension(40, 30));
+        container.add(fontBoldBox);
     }
 
     private void createImageIcon(String[] iconList, ImageIcon[] imageIcons) {
@@ -56,8 +83,8 @@ public class Buttons {
 
             if (iconList[i].equals("zoomIn") || iconList[i].equals("zoomOut")) {
                 width = 25; height = 25;
-            } else if (iconList[i].equals("rectangularBox1")) {
-                width = 100; height = 30;
+            } else if (iconList[i].equals("lineType")) {
+                width = 190; height = 45;
             } else { width = 30; height=30; }
 
             Image scaledImage = orgImag.getScaledInstance(width, height, Image.SCALE_SMOOTH);
