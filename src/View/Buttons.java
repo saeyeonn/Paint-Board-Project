a30d8f8179486c583547c07a1ff62b362d2ec539
saeyeon0522 <sy01lim@naver.com>
2023-09-28 22:29:01 +0900

    Refactor: refactor Buttons class

--- src/View/Buttons.java
+++ src/View/Buttons.java
@@ -5,55 +5,60 @@ import java.awt.*;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 
-public class Buttons {
+public class Buttons extends JButton{
     private ImageIcon[] imageIcons;
 
     public Buttons(Container container, String[] iconList, Frame frame) {
+        imageIcons = new ImageIcon[iconList.length];
+        createImageIcon(iconList);
         createButton(container, iconList, frame);
     }
 
-    public void createButton(Container container, String[] iconList, Frame frame) {
+    private void createImageIcon(String[] iconList) {
+        for (int i = 0; i< iconList.length; i++) {
+            int width = 30, height = 30;
 
-        imageIcons = new ImageIcon[iconList.length];
-        JButton[] buttons = new JButton[iconList.length];
+            if (iconList[i].equals("zoomIn") || iconList[i].equals("zoomOut")) { width = height = 25; }
+            if (iconList[i].equals("lineType")) { width = 190; height = 45; }
 
-        createImageIcon(iconList, imageIcons);
-        setButtons(container, frame, buttons, imageIcons, iconList);
-
+            Image scaledImage = new ImageIcon("src/ImageSource/" + iconList[i] + ".png")
+                    .getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
+            imageIcons[i] = new ImageIcon(scaledImage);
+        }
     }
 
-    private void setButtons(Container container, Frame frame, JButton[] buttons, ImageIcon[] imageIcons, String[] iconList) {
+    private void createButton(Container container, String[] iconList, Frame frame) {
+        JButton[] buttons = new JButton[iconList.length];
+
         for (int i = 0; i < buttons.length; i++) {
             buttons[i] = new JButton(imageIcons[i]);
-            container.add(buttons[i]);
-            buttons[i].setName("img-" + iconList[i]);
-
-            if (buttons[i].getName().equals("img-after"))
-                container.add(Box.createHorizontalStrut(5));
-            if (buttons[i].getName().equals("img-textBox"))
-                container.add(Box.createVerticalStrut(50));
-            if (buttons[i].getName().equals("img-ungroup") || buttons[i].getName().equals("img-circle"))
-                container.add(Box.createVerticalStrut(70));
-            container.add(Box.createHorizontalStrut(2));
-
-            if (buttons[i].getName().equals("img-lineType")) {
-                buttons[i].setPreferredSize(new Dimension(180, 45));
-                buttons[i].setBorderPainted(true);
-
+            buttons[i].setName("button-" + iconList[i]);
+            applyButtonsStyle(container, buttons[i]);
+            if (iconList[i].equals("more")) selectColor(buttons[i], frame);
+            if (buttons[i].getName().equals("button-lineType")) {
                 JComboBox<String> lineBoldComboBox = new JComboBox<>(new String[] {"line 3pt", "line 6pt", "line 9pt"});
                 lineBoldComboBox.setPreferredSize(new Dimension(180, 30));
                 container.add(lineBoldComboBox);
             }
-            else buttons[i].setPreferredSize(new Dimension(35, 35));
-
-            buttons[i].setFocusPainted(false);
-            buttons[i].setBorderPainted(false);
-            buttons[i].setContentAreaFilled(false);
-
-            clickAction(buttons[i], imageIcons[i]);
-            if (iconList[i].equals("more")) selectColor(buttons[i], frame);
+            container.add(buttons[i]);
+            clickAction(buttons[i]);
         }
         createTextBoxSetting(container);
+    }
+
+    private void applyButtonsStyle(Container container, JButton button) {
+        button.setBorderPainted(false);
+        button.setFocusPainted(false);
+        button.setContentAreaFilled(false);
+        container.add(Box.createHorizontalStrut(2));
+
+        if (button.getName().equals("button-after")) container.add(Box.createHorizontalStrut(5));
+        if (button.getName().equals("button-textBox")) container.add(Box.createVerticalStrut(50));
+        if (button.getName().equals("button-ungroup") || button.getName().equals("button-circle"))
+            container.add(Box.createVerticalStrut(70));
+
+        if (button.getName().equals("button-lineType")) { button.setPreferredSize(new Dimension(180, 45)); }
+        else button.setPreferredSize(new Dimension(35, 35));
     }
 
     private void createTextBoxSetting(Container container) {
@@ -73,27 +78,8 @@ public class Buttons {
         container.add(fontBoldBox);
     }
 
-    private void createImageIcon(String[] iconList, ImageIcon[] imageIcons) {
-
-        int width, height;
-
-        for (int i = 0; i< iconList.length; i++) {
-            imageIcons[i] = new ImageIcon("src/ImageSource/" + iconList[i] + ".png");
-            Image orgImag = imageIcons[i].getImage();
-
-            if (iconList[i].equals("zoomIn") || iconList[i].equals("zoomOut")) {
-                width = 25; height = 25;
-            } else if (iconList[i].equals("lineType")) {
-                width = 190; height = 45;
-            } else { width = 30; height=30; }
-
-            Image scaledImage = orgImag.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-            imageIcons[i] = new ImageIcon(scaledImage);
-        }
-    }
-
     // maybe should mv below 2 methods to other package while implementing controller
-    public void clickAction(JButton button, ImageIcon imageIcon) {
+    public void clickAction(JButton button) {
         button.addFocusListener(new FocusListener() {
             @Override
             public void focusGained(FocusEvent e) {
