8d2426332d8f10ab6be0c4b512ba4881bdd3ad41
saeyeon0522 <sy01lim@naver.com>
2023-09-29 01:09:39 +0900

    Add: line type and bold selection, font underline function Design: re-align icon Refactor: View/Buttons

--- src/View/Buttons.java
+++ src/View/Buttons.java
@@ -5,81 +5,99 @@ import java.awt.*;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 
-public class Buttons extends JButton{
-    private ImageIcon[] imageIcons;
+public class Buttons extends JButton {
+    JButton[] buttons;
 
-    public Buttons(Container container, String[] iconList, Frame frame) {
-        imageIcons = new ImageIcon[iconList.length];
-        createImageIcon(iconList);
-        createButton(container, iconList, frame);
-    }
-
-    private void createImageIcon(String[] iconList) {
-        for (int i = 0; i< iconList.length; i++) {
-            int width = 30, height = 30;
-
-            if (iconList[i].equals("zoomIn") || iconList[i].equals("zoomOut")) { width = height = 25; }
-            if (iconList[i].equals("lineType")) { width = 190; height = 45; }
-
-            Image scaledImage = new ImageIcon("src/ImageSource/" + iconList[i] + ".png")
-                    .getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
-            imageIcons[i] = new ImageIcon(scaledImage);
-        }
-    }
-
-    private void createButton(Container container, String[] iconList, Frame frame) {
-        JButton[] buttons = new JButton[iconList.length];
+    Buttons(Container container, String[] iconList, Frame frame) {
+        buttons = new JButton[iconList.length];
 
         for (int i = 0; i < buttons.length; i++) {
-            buttons[i] = new JButton(imageIcons[i]);
-            buttons[i].setName("button-" + iconList[i]);
-            applyButtonsStyle(container, buttons[i]);
-            if (iconList[i].equals("more")) selectColor(buttons[i], frame);
-            if (buttons[i].getName().equals("button-lineType")) { container.add(createLineBoldBox());
-
-            }
+            buttons[i] = (JButton) createButton(container, createImageIcon(iconList[i]), iconList[i], frame);
             container.add(buttons[i]);
-            clickAction(buttons[i]);
+            if (buttons[i].getName().equals("button-textBox") || buttons[i].getName().equals("button-circle"))
+                container.add(Box.createVerticalStrut(50));
         }
         createTextBoxSetting(container);
     }
 
-    private Component createLineBoldBox() {
-        JComboBox<String> lineBoldComboBox = new JComboBox<>(new String[] {"line 3pt", "line 6pt", "line 9pt"});
-        lineBoldComboBox.setPreferredSize(new Dimension(180, 30));
-        return lineBoldComboBox;
+    private ImageIcon createImageIcon(String iconName) {
+            int width = 25, height = 25;
+
+        if (iconName.equals("zoomIn") || iconName.equals("zoomOut")) {width = height = 25;}
+        if (iconName.equals("1pt")|| iconName.equals("3pt")|| iconName.equals("6pt") || iconName.equals("9pt")
+                || iconName.equals("dot") || iconName.equals("dash") || iconName.equals("solid")) {width = 70; height = 40;}
+        if (iconName.equals("lineType") || iconName.equals("lineBold")) {width = 190; height = 45;}
+        Image scaledImage = new ImageIcon("src/ImageSource/" + iconName + ".png")
+                .getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
+
+        return new ImageIcon(scaledImage);
+    }
+
+    private Component createButton(Container container, ImageIcon imageIcon, String iconList, Frame frame) {
+        JButton button = new JButton(imageIcon);
+        button.setName("button-" + iconList);
+        applyButtonsStyle(container, button);
+        if (button.getName().equals("button-more")) selectColor(button, frame);
+        if (button.getName().equals("button-lineEraser")) {createLineBox(container);}
+        clickAction(button);
+        return button;
     }
 
     private void applyButtonsStyle(Container container, JButton button) {
         button.setBorderPainted(false);
         button.setFocusPainted(false);
         button.setContentAreaFilled(false);
-        container.add(Box.createHorizontalStrut(2));
+        button.setPreferredSize(new Dimension(35, 35));
 
-        if (button.getName().equals("button-after")) container.add(Box.createHorizontalStrut(5));
-        if (button.getName().equals("button-textBox")) container.add(Box.createVerticalStrut(50));
-        if (button.getName().equals("button-ungroup") || button.getName().equals("button-circle"))
-            container.add(Box.createVerticalStrut(70));
-
-        if (button.getName().equals("button-lineType")) { button.setPreferredSize(new Dimension(180, 45)); }
-        else button.setPreferredSize(new Dimension(35, 35));
+        if (button.getName().equals("button-zoomIn")) container.add(Box.createHorizontalStrut(5));
     }
 
-    private void createTextBoxSetting(Container container) {
+    private void createLineBox(Container container) {
+        JLabel lineTypeLabel = new JLabel("Line Type");
+        lineTypeLabel.setPreferredSize(new Dimension(60, 30));
+        container.add(lineTypeLabel);
 
+        ImageIcon[] lineTypeIcons = {createImageIcon("dash"),
+                createImageIcon("dot"), createImageIcon("solid")};
+        JComboBox<ImageIcon> lineTypeBox = new JComboBox<>(lineTypeIcons);
+        lineTypeBox.setPreferredSize(new Dimension(80, 30));
+        container.add(lineTypeBox);
+        container.add(Box.createVerticalStrut(30));
+
+        JLabel lineBoldLabel = new JLabel("Line Bold");
+        lineBoldLabel.setPreferredSize(new Dimension(60, 30));
+        container.add(lineBoldLabel);
+
+        ImageIcon[] lineBoldIcons = {createImageIcon("9pt"), createImageIcon("6pt"),
+                createImageIcon("3pt"), createImageIcon("1pt")};
+        JComboBox<ImageIcon> lineBoldBox = new JComboBox<>(lineBoldIcons);
+        lineBoldBox.setPreferredSize(new Dimension(80, 30));
+        container.add(lineBoldBox);
+        container.add(Box.createVerticalStrut(50));
+
+    }
+
+
+    private void createTextBoxSetting(Container container) {
         JComboBox<String> fontComboBox = new JComboBox<>(new String[] {"Arial", "Times New Roman", "Courier New"});
-        fontComboBox.setPreferredSize(new Dimension(90, 30));
+        fontComboBox.setPreferredSize(new Dimension(100, 30));
         container.add(fontComboBox);
+        container.add(Box.createHorizontalStrut(5));
 
         SpinnerNumberModel fontSizeModel = new SpinnerNumberModel(12, 1, 80, 5);
         JSpinner fontSizeSpinner = new JSpinner(fontSizeModel);
-        fontSizeSpinner.setPreferredSize(new Dimension(45, 30));
+        fontSizeSpinner.setPreferredSize(new Dimension(35, 30));
         container.add(fontSizeSpinner);
 
-        JCheckBox fontBoldBox = new JCheckBox("B");
+        JCheckBox fontBoldBox = new JCheckBox("Bold");
         fontBoldBox.setOpaque(false);
-        fontBoldBox.setPreferredSize(new Dimension(40, 30));
+        fontBoldBox.setPreferredSize(new Dimension(60, 30));
         container.add(fontBoldBox);
+
+        JCheckBox fontUnderLineBox = new JCheckBox("Underline");
+        fontUnderLineBox.setOpaque(false);
+        fontUnderLineBox.setPreferredSize(new Dimension(90, 30));
+        container.add(fontUnderLineBox);
     }
 
     // maybe should mv below 2 methods to other package while implementing controller
@@ -100,7 +118,7 @@ public class Buttons extends JButton{
 
     public void selectColor(JButton moreButton, Frame frame) {
         moreButton.addActionListener(e -> {
-            Color selectedColor = JColorChooser.showDialog(frame, "Color Chooser", Color.LIGHT_GRAY);
+            Color rgbColor = JColorChooser.showDialog(frame, "Color Chooser", Color.LIGHT_GRAY);
         });
     }
 }
--- src/View/ToolBar.java
+++ src/View/ToolBar.java
@@ -6,11 +6,11 @@ import java.awt.*;
 public class ToolBar extends JToolBar {
         private static final String[] iconList = {"save", "open", "group", "ungroup", "red", "yellow", "green",
                 "backgroundColor", "blue", "brown", "grey", "shapeColor", "black", "white", "more", "deleteAll",
-                "line", "square", "triangle", "circle", "lineEraser", "pixelEraser", "cut", "paste", "lineType",
+                "line", "square", "triangle", "circle", "lineEraser", "pixelEraser", "cut", "paste",
                 "pencil", "brush", "spray", "textBox"};
 
         public ToolBar(Frame frame) {
-                setPreferredSize(new Dimension(220, 400));
+                setPreferredSize(new Dimension(190, 300));
                 setBackground(new Color(222, 237, 239));
                 setLayout(new FlowLayout());
                 Buttons buttons = new Buttons(this, iconList, frame);
@@ -20,14 +20,14 @@ public class ToolBar extends JToolBar {
         protected void paintComponent(Graphics graphics) {
                 super.paintComponent(graphics);
 
-                int colStartX = 20, colStartY = 75, colWidth = 137, colHeight = 128;
+                int colStartX = 20, colStartY = 44, colWidth = 120, colHeight = 118;
                 int colArcWidth = 7, colArcHeight = 7;
 
                 graphics.setColor(Color.BLACK);
                 ((Graphics2D) graphics).setStroke(new BasicStroke(1));
                 graphics.drawRoundRect(colStartX, colStartY, colWidth, colHeight, colArcWidth, colArcHeight);
 
-                int shpStartX = 20, shpStartY = 212, shpWidth = 182, shpHeight = 45;
+                int shpStartX = 20, shpStartY = 170, shpWidth = 160, shpHeight = 40;
                 int shpArcWidth = 7, shpArcHeight = 7;
 
                 graphics.setColor(Color.BLACK);
