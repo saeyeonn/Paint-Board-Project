10144c7378a3d6dc35e7e55f82d5e718a26e08ed
OhHuijin <145532762+OhHuijin@users.noreply.github.com>
2023-12-01 16:34:24 +0900

    Add files via upload

---
+++ window/TextSpinnerForm.java
@@ -0,0 +1,20 @@
+package window;
+
+import action.ChangeDetector;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TextSpinnerForm extends JSpinner{
+    private static final TextSpinnerForm fontSizeSpinner =
+            new TextSpinnerForm(new SpinnerNumberModel(12, 1, 80, 5), 45, 30);
+
+    TextSpinnerForm(SpinnerNumberModel spinnerNumberModel, int width, int height) { //set details
+        setPreferredSize(new Dimension(width, height));
+        addChangeListener(new ChangeDetector());
+      }
+
+    public static TextSpinnerForm getFontSizeSpinner() {
+        return fontSizeSpinner;
+    }
+}
