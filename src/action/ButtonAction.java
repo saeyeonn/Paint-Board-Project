4ad23054b7522e06440d00dc070f848bc3377d85
TPRPT <pinia124@naver.com>
2023-12-03 00:26:00 +0900

    doing과 zoom 진행중입니다..

--- src/action/ButtonAction.java
+++ src/action/ButtonAction.java
@@ -3,6 +3,7 @@ package action;
 import drawing.Pen;
 import text.TextBox;
 
+import javax.print.attribute.standard.MediaSize.NA;
 import javax.swing.*;
 
 import java.awt.event.ActionEvent;
@@ -22,7 +23,14 @@ public class ButtonAction implements ActionListener {
         } else if (Name.equals("28_textBox")) {
             TextBox textBox = new TextBox();
             textBox.setCreating();
+        } else if (Name.equals("redo")) {
+            System.out.println("리두");
+        } else if (Name.equals("undo")) {
+            System.out.println("언두");
+        } else if (Name.equals("zoomIn")) {
+            System.out.println("줌인");
+        } else if (Name.equals("zoomOut")) {
+            System.out.println("줌아웃");
         }
     }
-
 }
