f093d8b1a0cd4d6093dc39644fbf15f8c6f904ec
no-glass-otacku <ga7262681@gmail.com>
2023-12-03 07:46:57 +0900

    버튼 if문 제거 (충돌 방지)

--- src/action/ButtonAction.java
+++ src/action/ButtonAction.java
@@ -15,15 +15,7 @@ public class ButtonAction implements ActionListener {
         JButton selectButton = (JButton) e.getSource();
 
         String Name=selectButton.getName();
-        if (Name.equals("25_pen")) {
-            // Pen 버튼을 눌렀을 때 
-            System.out.println("Pen!!");
-            Pen pen= new Pen();
-//            pen.draw((Button) selectButton);
-        } else if (Name.equals("")) {
-            //쭉 밑으로 버튼마다의 기능 연결
-
-        }
+        
     }
 
 }
