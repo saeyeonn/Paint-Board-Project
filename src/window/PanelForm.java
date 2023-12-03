8b1d37d07b2908ec93e560290f757a986271826c
TPRPT <pinia124@naver.com>
2023-12-04 01:44:36 +0900

    줌 구현중...

--- src/window/PanelForm.java
+++ src/window/PanelForm.java
@@ -3,20 +3,26 @@ package window;
 import javax.swing.*;
 
 import text.TextBox;
+import zoom.Zoom;
 
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 
 public class PanelForm extends JPanel{
     private TextBox textBox;
+    private Zoom zoom;
 
     public PanelForm() {
         add(MiniBarForm.getInstance(), BorderLayout.NORTH); // add minibar
         setBackground(Color.WHITE);
         setLayout(new FlowLayout());
 
-        textBox = new TextBox(PanelForm.this);
+        textBox = new TextBox(this);
+        zoom = new Zoom();
+    
 
         addMouseListener(new MouseListener() {
             @Override
@@ -45,5 +51,23 @@ public class PanelForm extends JPanel{
 
             }
         });
+
+        // 마우스 휠 리스너 추가
+        addMouseWheelListener(new MouseWheelListener() {
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                // 마우스 휠 움직임에 따라 zoom 처리
+                int notches = e.getWheelRotation();
+                if (notches < 0) {
+                    // 마우스 휠을 위로 돌리면 줌 인
+                    zoom.zoomIn();
+                } else {
+                    // 마우스 휠을 아래로 돌리면 줌 아웃
+                    zoom.zoomOut();
+                }
+                repaint();
+            }
+        });
+
     }
 }
