f46da33491b4fe02db1bee88c2132216484256a3
TPRPT <pinia124@naver.com>
2023-12-03 17:31:39 +0900

    폰트 사이즈 변경이 좀 애매하긴 한데 시간이 없어서 텍스트 최종입니다

--- src/text/FontType.java
+++ src/text/FontType.java
@@ -11,7 +11,8 @@ import java.util.Map;
 
 public class FontType {
     private static JTextField selectedTextField = null; // 현재 선택된 TB 저장
-    private static Map<JTextField, TextStates> FontStatesMap = new HashMap<>(); // 각 TB의 체크 표시 설정 저장
+    private static Map<JTextField, States> statesMap = TextMaps.statesMap;
+    // private static Map<JTextField, TextStates> FontStatesMap = new HashMap<>(); // 각 TB의 체크 표시 설정 저장
 
     private static TextComboBoxForm fontComboBoxForm;
 
@@ -35,8 +36,8 @@ public class FontType {
         applySelectedAttributes(); // 가져온 체크 박스 설정을 바탕으로 폰트 스타일 조절
 
         // 각 TB에 대한 체크 박스 설정 객체를 생성하여 맵에 추가
-        if (!FontStatesMap.containsKey(textField)) {
-            FontStatesMap.put(textField, new TextStates());
+        if (!statesMap.containsKey(textField)) {
+            statesMap.put(textField, new States());
         }
     }
 
@@ -44,7 +45,7 @@ public class FontType {
     private static void applySelectedAttributes() {
         // 선택된 TB가 있는 경우에만 작업 실행
         if (selectedTextField != null) {
-            TextStates FontStates = FontStatesMap.get(selectedTextField);
+            States FontStates = statesMap.get(selectedTextField);
     
             if (FontStates != null) { // null 체크 추가
                 Font font = selectedTextField.getFont();
@@ -53,18 +54,15 @@ public class FontType {
                 if (FontStates.getFontName() != null) {
                     name = (String) TextComboBoxForm.getFontComboBox().getSelectedItem(); // 선택한 폰트 가져오기
                 }
-    
-                Font newFont = new Font(name, font.getStyle(), font.getSize());
-                selectedTextField.setFont(newFont);  // 그 폰트 객체에 맞게 새로운 글꼴 적용
-    
-                // Underline 설정 독립적으로 적용
+
                 Map<TextAttribute, Object> attributes = new HashMap<>();
                 if (FontStates.isUnderline()) {
                     attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
                 }
     
-                newFont = newFont.deriveFont(attributes);
-                selectedTextField.setFont(newFont);
+                Font newFont = new Font(name, font.getStyle(), font.getSize()).deriveFont(attributes);
+                System.out.println("폰트 변경.."+FontStates.isBold()+FontStates.isUnderline());
+                selectedTextField.setFont(newFont);  // 그 폰트 객체에 맞게 새로운 글꼴 적용
             }
         }
     }
@@ -72,10 +70,10 @@ public class FontType {
     // 현재 설정 저장
     private static void storeFontType(JTextField textField) {
         if (textField != null && fontComboBoxForm != null) {
-            TextStates FontStates = FontStatesMap.get(textField);
+            States FontStates = statesMap.get(textField);
             if (FontStates == null) {
-                FontStates= new TextStates();
-                FontStatesMap.put(textField, FontStates);
+                FontStates= new States();
+                statesMap.put(textField, FontStates);
             }
             FontStates.setFontName((String) fontComboBoxForm.getSelectedItem());
             System.out.println("폰트 저장 완료 "+ (String) fontComboBoxForm.getSelectedItem());
@@ -85,7 +83,7 @@ public class FontType {
     // 폰트 타입 로드하여 선택 상태 업데이트
     private static void loadFontType() {
         if (selectedTextField != null) {
-            TextStates FontStates = FontStatesMap.get(selectedTextField);
+            States FontStates = statesMap.get(selectedTextField);
             if (FontStates != null) { // null 체크 추가
                 fontComboBoxForm.setSelectedItem(FontStates.getFontName());
             }
