ddc45a9e45fef90286a9db3b9298c4dd02079798
saeyeon0522 <sy01lim@naver.com>
2023-12-04 12:46:59 +0900

    [Add] 선 그리기 구현

--- src/shape/Shape.java
+++ src/shape/Shape.java
@@ -7,15 +7,17 @@ public class Shape {
     private int y;
     private int width;
     private int height;
+    private boolean isLine;
     private boolean isRectangle;
     private boolean isTriangle;
     private boolean isCircle;
 
-    public Shape(int x, int y, int width, int height, boolean isRectangle, boolean isTriangle, boolean isCircle) {
+    public Shape(int x, int y, int width, int height, boolean isLine, boolean isRectangle, boolean isTriangle, boolean isCircle) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
+        this.isLine = isLine;
         this.isRectangle = isRectangle;
         this.isTriangle = isTriangle;
         this.isCircle = isCircle;
@@ -30,6 +32,8 @@ public class Shape {
             g.drawPolygon(xPoints, yPoints, 3);
         } else if (isCircle) {
             g.drawOval(x, y, width, height);
+        } else if (isLine) {
+            g.drawLine(x, y, x + width, y + height);
         }
     }
 }
